15f1ec3d2a1ea2b4319462c4e49bd8d518e43337
Asheley McDonald <asheleymcdonald@gmail.com>
2018-04-02 00:48:15 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,19 @@
 class Person
-
-  def initialize(name)
+  
+  def name=(name)
     @name = name
   end
-
+  
   def name
     @name
   end
 
-  def name=(new_name)
-    @name = new_name
-  end
+  def job=(job)
+    @job = job
+  end 
 
+  def job 
+    @job
+  end 
+  
 end
